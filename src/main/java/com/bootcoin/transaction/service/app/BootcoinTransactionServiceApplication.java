30648ca609b7796e604dc341e46f362705b997b4
Jorge Andres Espinoza Canales <jorge.espinoza.c22@gmail.com>
2022-05-12 16:07:35 -0500

    kaf

---
+++ src/main/java/com/bootcoin/transaction/service/app/BootcoinTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bootcoin.transaction.service.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcoinTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcoinTransactionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcoin/transaction/service/app/BootcoinTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcoin.transaction.service.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcoinTransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
